63fdf5d37aa8c62cd67d8e60d127feb2d627a5c9
Vox Humana <jonprine@gmail.com>
2020-08-23 16:25:57 -0500

    connection to database

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+const cTable = require('console.table');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_tracker'
+  });
+   
+
+  
+  connection.end();
